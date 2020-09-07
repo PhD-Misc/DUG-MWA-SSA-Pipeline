afaad85c5f13069f51770a00d92ffd1cb8ed4c89
steveprabu <steverajprabu@gmail.com>
2020-09-07 14:13:16 +0800

    now searches over a 70 km cone radius

--- bin/satSearch.sh
+++ bin/satSearch.sh
@@ -1,13 +1,14 @@
-#! /bin/bash -l
+#!/bin/bash  
 #SBATCH --export=NONE
 #SBATCH -M zeus
 #SBATCH -p workq
-#SBATCH --time=4:00:00
+#SBATCH --time=16:00:00
 #SBATCH --ntasks=12
 #SBATCH --mem=20GB
 #SBATCH -J satSearch
 #SBATCH --mail-type FAIL,TIME_LIMIT,TIME_LIMIT_90
 #SBATCH --mail-user sirmcmissile47@gmail.com
+#SBATCH -q high
 
 start=`date +%s`
 
@@ -26,44 +27,69 @@ cd ${datadir}
 
 for ((i=0;i<10;i++));
 do
-    rm *${i}-dirty.fits
+    rm *${i}-image.fits
 done
 
 ## run sat search and run dep jobs
 
-satSearch.py --obs ${obsnum} --t1 1 --t2 55 --user ${spaceTrackUser} --passwd ${spaceTrackPassword} --debug=true
+satSearch.py --obs ${obsnum} --t1 1 --t2 55 --user "steverajprabu@gmail.com" --passwd "Qwertyuiop1234567890" --debug=True
 
 Tvar=$(<t.txt)
-array=(`echo $Tvar | sed 's/ /\n/g'`)
+array1=(`echo $Tvar | sed 's/ /\n/g'`)
+array=()
+for m in "${array1[@]}";
+do
+    if [[ ! " ${array[@]} " =~ ${m} ]];
+    then
+        array+=(${m})
+    fi
+
+    n=$((m+1))
+    if [[ ! " ${array[@]} " =~ ${n} ]];
+    then
+        array+=(${n})
+    fi
+
+    p=$((m-1))
+    if [[ ! " ${array[@]} " =~ ${p} ]];
+    then
+        array+=(${p})
+    fi
+done
+
+
 
 ## submit high angular resolution jobs
 
-cd ${base}
-channels=768
-job_array=""
-for q in "${array[@]}";
-do
+# cd ${base}
+# channels=768
+# job_array=""
+# #for q in "${array[@]}";
+# for q in $(seq 55)
+# do
 
-    # submit job for timestep 
-    script="${base}queue/args_hrimage_${obsnum}.sh"
-    cat ${base}bin/arg_hrimage.sh | sed -e "s:OBSNUM:${obsnum}:g" \
-                                -e "s:BASE:${base}:g"\
-                                -e "s:CHANNELS:${channels}:g"\
-                                -e "s:TIMESTEP:${timeStep}:g" > ${script}
-    output="${base}queue/logs/args_hrimage_${obsnum}.o%A"
-    error="${base}queue/logs/args_hrimage_${obsnum}.e%A"
-    sub="sbatch --begin=now+15 --output=${output} --error=${error} -A pawsey0345 ${script} "
-    jobid=($(${sub}))
-    jobid=${jobid1[3]}
+#    # submit job for timestep 
+#    script="${base}queue/args_hrimage_${obsnum}_timeStep${q}.sh"
+#    cat ${base}bin/arg_hrimage.sh > ${script}
+#    output="${base}queue/logs/args_hrimage_${obsnum}_timeStep${q}.o%A"
+#    error="${base}queue/logs/args_hrimage_${obsnum}_timeStep${q}.e%A"
+#    sub="sbatch --begin=now+15 --output=${output} --error=${error} -A pawsey0345 ${script} -o ${obsnum} -b ${base} -t ${q}"
+#    jobid=($(${sub}))
+#    jobid=${jobid[3]}
 
-    # rename the err/output files as we now know the jobid
-    error=`echo ${error} | sed "s/%A/${jobid}/"`
-    output=`echo ${output} | sed "s/%A/${jobid}/"`
-    job_array=${job_array}:${jobid}
- 
-done
+#    # rename the err/output files as we now know the jobid
+#    error=`echo ${error} | sed "s/%A/${jobid}/"`
+#    output=`echo ${output} | sed "s/%A/${jobid}/"`
+#    job_array=${job_array}:${jobid}
 
-echo "the dependent job arrray is " ${job_array:1}
+# done
+#
+#echo "the dependent job arrray is " ${job_array:1}
+
+## submit jobs for obrinfo
+ 
+
+## submit jobs to clear files
 
 end=`date +%s`
 runtime=$((end-start))
