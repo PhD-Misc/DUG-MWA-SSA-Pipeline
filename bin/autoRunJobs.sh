268f3ca642a427799e7f0b866df4061d68a60c05
steveprabu <steverajprabu@gmail.com>
2020-09-15 11:02:32 +0800

    updated to do asvo jobs as well

--- bin/autoRunJobs.sh
+++ bin/autoRunJobs.sh
@@ -2,11 +2,12 @@
 
 usage()
 {
-echo "autoSubmitJobs.sh [-c calibration] [-o obsnum] [-a account] [-f channels] [-s timeSteps]
+echo "autoSubmitJobs.sh [-c calibration] [-o obsnum] [-a account] [-f channels] [-s timeSteps] [-d download link]
         -c calibration          : path to calibration solution
         -a account              : pawsey Account to use
         -f channels             : the number of channels in ms, default=768
         -s timeSteps            : the number of timeSteps in ms, default=56
+        -d download link        : the ASVO link to download observation
         -o obsnum               : the obsid" 1>&2;
 exit 1;
 }
@@ -16,10 +17,14 @@ account="mwasci"
 channels=768
 timeSteps=55
 obsnum=
+link=
 
-while getopts 'c:o:a:f:s:' OPTION
+while getopts 'c:o:a:f:s:d:' OPTION
 do
     case "$OPTION" in
+        d)
+            link=${OPTARG}
+            ;;
         f)
             channels=${OPTARG}
             ;;
@@ -49,13 +54,25 @@ fi
 
 base=/astro/mwasci/sprabu/satellites/DUG-MWA-SSA-Pipeline/
 
+### submit the download job 
+script="${base}queue/asvo_${obsnum}.sh"
+cat ${base}bin/asvo.sh | sed -e "s:OBSNUM:${obsnum}:g" \
+                                -e "s:BASE:${base}:g" > ${script}
+output="${base}queue/logs/asvo_${obsnum}.o%A"
+error="${base}queue/logs/asvo_${obsnum}.e%A"
+sub="sbatch --begin=now+15 --output=${output} --error=${error} ${script} -l ${link} "
+jobid0=($(${sub}))
+jobid0=${jobid0[3]}
+
+echo "Submitted asvo job as ${jobid0}"
+
 ## run cotter ##
 script="${base}queue/cotter_${obsnum}.sh"
 cat ${base}/bin/cotter.sh | sed -e "s:OBSNUM:${obsnum}:g" \
                                  -e "s:BASE:${base}:g" > ${script}
 output="${base}queue/logs/cotter_${obsnum}.o%A"
 error="${base}queue/logs/cotter_${obsnum}.e%A"
-sub="sbatch --begin=now+15 --output=${output} --error=${error} -A ${account} ${script} -c ${calibrationPath} "
+sub="sbatch --begin=now+15 --output=${output} --error=${error} --dependency=afterok:${jobid0} -A ${account} ${script} -c ${calibrationPath} "
 jobid1=($(${sub}))
 jobid1=${jobid1[3]}
 # rename the err/output files as we now know the jobid
@@ -97,20 +114,20 @@ output=`echo ${output} | sed "s/%A/${jobid3}/"`
 echo "Submitted RFISeeker job as ${jobid3}"
 
 
-## run satSeach job ##
-script="${base}queue/satSearch_${obsnum}.sh"
-cat ${base}/bin/satSearch.sh | sed -e "s:OBSNUM:${obsnum}:g" \
-                                 -e "s:BASE:${base}:g" > ${script}
-output="${base}queue/logs/satSearch_${obsnum}.o%A"
-error="${base}queue/logs/satSearch_${obsnum}.e%A"
-sub="sbatch --begin=now+15 --output=${output} --error=${error} -A ${account} --dependency=afterok:${jobid3} ${script}"
-jobid4=($(${sub}))
-jobid4=${jobid4[3]}
-## rename the err/output files as we now know the jobid
-error=`echo ${error} | sed "s/%A/${jobid4}/"`
-output=`echo ${output} | sed "s/%A/${jobid4}/"`
+# ## run satSeach job ##
+# script="${base}queue/satSearch_${obsnum}.sh"
+# cat ${base}/bin/satSearch.sh | sed -e "s:OBSNUM:${obsnum}:g" \
+#                                  -e "s:BASE:${base}:g" > ${script}
+# output="${base}queue/logs/satSearch_${obsnum}.o%A"
+# error="${base}queue/logs/satSearch_${obsnum}.e%A"
+# sub="sbatch --begin=now+15 --output=${output} --error=${error} -A ${account} --dependency=afterok:${jobid3} ${script}"
+# jobid4=($(${sub}))
+# jobid4=${jobid4[3]}
+# ## rename the err/output files as we now know the jobid
+# error=`echo ${error} | sed "s/%A/${jobid4}/"`
+# output=`echo ${output} | sed "s/%A/${jobid4}/"`
 
-echo "Submitter satSearch job as ${jobid4}"
+# echo "Submitter satSearch job as ${jobid4}"
 
 
 
